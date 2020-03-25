75f6be943638d2df7a5631dd07edc8c989021bf8
araiknikoghosyan <araiknikogosyan10@gmail.com>
2020-03-25 21:44:00 +0400

    Add new Function by Factorial and Fibonachi

---
+++ CalcApp_Foem/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CalcApp_Foem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.Divade = new System.Windows.Forms.Button();
+            this.Clear_Button = new System.Windows.Forms.Button();
+            this.Fact_button = new System.Windows.Forms.Button();
+            this.Fibo_button = new System.Windows.Forms.Button();
+            this.Pow_button = new System.Windows.Forms.Button();
+            this.Sqrt_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(270, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(16, 93);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(75, 23);
+            this.SumButton.TabIndex = 4;
+            this.SumButton.Text = "-";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // Divade
+            // 
+            this.Divade.Location = new System.Drawing.Point(109, 63);
+            this.Divade.Name = "Divade";
+            this.Divade.Size = new System.Drawing.Size(75, 23);
+            this.Divade.TabIndex = 5;
+            this.Divade.Text = "/";
+            this.Divade.UseVisualStyleBackColor = true;
+            this.Divade.Click += new System.EventHandler(this.Divade_Click);
+            // 
+            // Clear_Button
+            // 
+            this.Clear_Button.Location = new System.Drawing.Point(109, 92);
+            this.Clear_Button.Name = "Clear_Button";
+            this.Clear_Button.Size = new System.Drawing.Size(75, 23);
+            this.Clear_Button.TabIndex = 6;
+            this.Clear_Button.Text = "Clear";
+            this.Clear_Button.UseVisualStyleBackColor = true;
+            this.Clear_Button.Click += new System.EventHandler(this.Clear_Button_Click);
+            // 
+            // Fact_button
+            // 
+            this.Fact_button.Location = new System.Drawing.Point(16, 122);
+            this.Fact_button.Name = "Fact_button";
+            this.Fact_button.Size = new System.Drawing.Size(75, 23);
+            this.Fact_button.TabIndex = 7;
+            this.Fact_button.Text = "Fact";
+            this.Fact_button.UseVisualStyleBackColor = true;
+            this.Fact_button.Click += new System.EventHandler(this.Fact_button_Click);
+            // 
+            // Fibo_button
+            // 
+            this.Fibo_button.Location = new System.Drawing.Point(109, 121);
+            this.Fibo_button.Name = "Fibo_button";
+            this.Fibo_button.Size = new System.Drawing.Size(75, 23);
+            this.Fibo_button.TabIndex = 8;
+            this.Fibo_button.Text = "Fibo";
+            this.Fibo_button.UseVisualStyleBackColor = true;
+            this.Fibo_button.Click += new System.EventHandler(this.Fibo_button_Click);
+            // 
+            // Pow_button
+            // 
+            this.Pow_button.Location = new System.Drawing.Point(16, 151);
+            this.Pow_button.Name = "Pow_button";
+            this.Pow_button.Size = new System.Drawing.Size(75, 23);
+            this.Pow_button.TabIndex = 9;
+            this.Pow_button.Text = "Pow";
+            this.Pow_button.UseVisualStyleBackColor = true;
+            this.Pow_button.Click += new System.EventHandler(this.Pow_button_Click);
+            // 
+            // Sqrt_Button
+            // 
+            this.Sqrt_Button.Location = new System.Drawing.Point(109, 151);
+            this.Sqrt_Button.Name = "Sqrt_Button";
+            this.Sqrt_Button.Size = new System.Drawing.Size(75, 23);
+            this.Sqrt_Button.TabIndex = 10;
+            this.Sqrt_Button.Text = "Sqrt";
+            this.Sqrt_Button.UseVisualStyleBackColor = true;
+            this.Sqrt_Button.Click += new System.EventHandler(this.Sqrt_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 225);
+            this.Controls.Add(this.Sqrt_Button);
+            this.Controls.Add(this.Pow_button);
+            this.Controls.Add(this.Fibo_button);
+            this.Controls.Add(this.Fact_button);
+            this.Controls.Add(this.Clear_Button);
+            this.Controls.Add(this.Divade);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button Divade;
+        private System.Windows.Forms.Button Clear_Button;
+        private System.Windows.Forms.Button Fact_button;
+        private System.Windows.Forms.Button Fibo_button;
+        private System.Windows.Forms.Button Pow_button;
+        private System.Windows.Forms.Button Sqrt_Button;
+    }
+}
+
---
+++ CalcApp_Foem/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcApp_Foem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Sum(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Sub(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+        static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        private void Divade_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Divade_(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+        static double Divade_(double a, double b)
+        {
+            if (b == 0)
+            {
+                MessageBox.Show("На ноль делить нелзья");
+            }
+            return a / b;
+        }
+
+        private void Clear_Button_Click(object sender, EventArgs e)
+        {
+            if (label1.Text != null && textBox1.Text != null && textBox2 != null)
+            {
+                label1.Text = "";
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+        private void Fact_button_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Fact(Convert.ToDouble(textBox1.Text)));
+        }
+        static double Fact(double d)
+        {
+            return d == 0 ? 1 : d * Fact(d - 1);
+        }
+
+        private void Fibo_button_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Fibo(Convert.ToDouble(textBox1.Text)));
+        }
+        static double Fibo(double d)
+        {
+            return d <= 2 ? 1 : Fibo(d - 1) + Fibo(d - 2);
+        }
+
+        private void Pow_button_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Pow(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text)));
+        }
+        static double Pow(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        private void Sqrt_Button_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(Sqrt(Convert.ToDouble(textBox1.Text)));
+        }
+
+        static double Sqrt(double d)
+        {
+            return Math.Sqrt(d);
+        }
+    }
+}
